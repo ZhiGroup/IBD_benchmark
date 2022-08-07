9762d75419c0e8c06f471aa62a93d6bcfcf33f5f
BennyUCF <45030064+BennyUCF@users.noreply.github.com>
2022-08-07 11:23:38 -0400

    Update siteCoverage.cs

--- IBD_Benchmark/siteCoverage.cs
+++ IBD_Benchmark/siteCoverage.cs
@@ -23,9 +23,9 @@ namespace IBD_BM
         /// </summary>
         public static void singleRun()
         {
-            string vcf_Path = "E:\\s15in\\mix.arr.e0.001.vcf";
-            string IBD_result_Path = "E:\\s15in\\mix.gt.arr.txt";
-            string outPath="E:\\tem\\dummy.out.txt";
+            string vcf_Path = "your vcf file path";
+            string IBD_result_Path = "your IBD tool's result file";
+            string outPath="output path";
 
             Dictionary<long, List<Loader.IBD_Phy_Start_End>> IBDs = Loader.Load_IBD(Loader.dataType.GroudTruth, IBD_result_Path);
             utl.siteDict siteD = new utl.siteDict(vcf_Path);
